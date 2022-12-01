bae65a57c377404db115dfdf99f0ff1807f51b47
Ndumatt1 <ndubuisimathew8@gmail.com>
2022-12-01 21:35:04 +0100

    bug fix on 12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
-number = 1
-while number <= 100:
-    if number % 3 == 0:
-        #continue
-        print('Fizz ', end="")
-    elif number % 5 == 0:
-        #continue
-        print('Buzz ', end="")
-    elif number % 3 == 0 and number % 5 == 0:
-        #continue
-        print('FizzBuzz ', end="")
-    else:
-        print("{} ".format(number), end="")
-    number = number + 1
+def fizzbuzz():
+    number = 1
+    while number <= 100:
+
+        if number % 3 == 0:
+            print('Fizz ', end="")
+
+        elif number % 5 == 0:
+            print('Buzz ', end="")
+
+        elif number % 3 == 0 and number % 5 == 0:
+            print('FizzBuzz ', end="")
+
+        else:
+            print("{} ".format(number), end="")
+
+        number = number + 1
