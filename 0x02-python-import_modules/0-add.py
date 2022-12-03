b7fa685eb93a72fe29a965437d98ab8b3117dfee
Ndumatt1 <ndubuisimathew8@gmail.com>
2022-12-03 17:32:58 +0100

    fixed bug on 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,11 +2,11 @@
 
 if __name__ == '__main__':
 
-    from add_0 import add
+    import add_0
 
     a = 1
     b = 2
-    result = a + b
-    add(a, b)
+    result = add_0.add(a, b)
+    #add(a, b)
 
     print("{:d} + {:d} = {:d}".format(a, b, result))
