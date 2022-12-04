fec1dc4b31ee7e60d848abe327432561de63e8a1
Ndumatt1 <ndubuisimathew8@gmail.com>
2022-12-04 17:11:23 +0100

    bug fix on 5-print_comb2 and 6-print_comb3

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
 for number in range(0, 100):
-
-    if number -1:
-        sep = ", "
-
-    print("{:02d}{}".format(number, sep), end="")
+    if number != 99:
+        print("{:02d}".format(number), end=", ")
+    else:
+        print("{:02d}".format(number))
