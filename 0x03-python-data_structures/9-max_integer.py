0a6e355c5bf153ee982467a97dc3b7279808cdd2
Ndumatt1 <ndubuisimathew8@gmail.com>
2022-12-06 17:10:17 +0100

    bug fix on 9-max_max_integer

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     if my_list == []:
-         return None
+        return None
     else:
         res = my_list[0]
         for index in my_list:
             if index > res:
                 res = index
         return res
-
