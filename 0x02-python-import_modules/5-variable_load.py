04d11db8de6c27fedd9825ffcf62557827953151
Ndumatt1 <ndubuisimathew8@gmail.com>
2022-12-06 17:46:25 +0100

    fixed bug on 5-variable_load.py

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
-    import variable_load_5
-
-    new_var = variable_load_5.a
-
-    print("{}".format(new_var))
+    from variable_load_5 import a
+    print("{:d}".format(a))
