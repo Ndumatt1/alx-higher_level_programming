bc3e157c95a68e7472a7652d9f7f7ad98cee8d43
Ndumatt1 <ndubuisimathew8@gmail.com>
2022-12-06 17:35:49 +0100

    bug fix on 3-infinite_add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,13 +3,11 @@ if __name__ == '__main__':
 
     from sys import argv
 
-    number = len(argv)
-
-    if number == 1:
+    if len(argv) == 1:
         add = 0
     else:
-        for i in range(1, number):
-            add = 0
+        add = 0
+        for i in range(1, len(argv)):
             num = int(argv[i])
             add += num
-            print("{:d}".format(add))
+        print("{:d}".format(add))
