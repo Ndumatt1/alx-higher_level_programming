a87b0badca9388d297ebc9c43d88c0ee7ee94578
Ndumatt1 <ndubuisimathew8@gmail.com>
2022-12-28 21:36:46 +0100

    upload 1-square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+class Square:
+    """ This class defines a square by private instance attribute: size
+    instantiation with size """
+    def __init__(self, size):
+        self.__size = size
