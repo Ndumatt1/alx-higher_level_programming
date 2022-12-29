fc756dedfbd8e3e9e12ac7a7dba3a94c5b148690
Ndumatt1 <ndubuisimathew8@gmail.com>
2022-12-29 12:52:26 +0100

    pycodestyle compliance

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,12 +4,10 @@
 
 class Square:
     """ Defines a square. """
-
-
     def __init__(self, size=0):
         """ Instantiates private attribute. """
         self.__size = size
         if int(size) is not size:
-            raise TypeError ("size must be an integer")
+            raise TypeError("size must be an integer")
         if size < 0:
-            raise ValueError ("size must be >= 0")
+            raise ValueError("size must be >= 0")
