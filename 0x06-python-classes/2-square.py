fe40acf81555f3d606d037efecf7eadb1f4324da
Ndumatt1 <ndubuisimathew8@gmail.com>
2022-12-29 12:40:40 +0100

    upload 2-square

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" This module defines a square. """
+
+
+class Square:
+    """ Defines a square. """
+
+
+    def __init__(self, size=0):
+        """ Instantiates private attribute. """
+        self.__size = size
+        if int(size) is not size:
+            raise TypeError ("size must be an integer")
+        if size < 0:
+            raise ValueError ("size must be >= 0")
