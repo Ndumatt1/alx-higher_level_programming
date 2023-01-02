cb6a09734ba493b55b090467be058ff10b8fabbc
Ndumatt1 <ndubuisimathew8@gmail.com>
2023-01-02 20:48:03 +0100

    upload 1-rectangle

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" This module writes a class Rectangle that defines a rectangle. """
+
+
+class Rectangle:
+    """ writes a class Rectangle that defines a rectangle. """
+    def __init__(self, width=0, height=0):
+        """ Instantiates private instance attribute width and height. """
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """ getter function to retrieve private instance attribute width."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ setter function to set the value of width. """
+        if not isinstance(self.__width, int):
+            raise TypeError('width must be an integer')
+        if self.__width < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = value
+
+    @property
+    def height(self):
+        """ getter function to retrieve the value of height. """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ setter function to set the value of height. """
+        if not isinstance(self.__height, int):
+            raise TypeError('height must be an integer')
+        if self.__height < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = value
