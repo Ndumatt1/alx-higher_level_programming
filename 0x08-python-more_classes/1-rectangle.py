c945ca5ad875f124c9e6fd40076bdbe6062cf6ed
Ndumatt1 <ndubuisimathew8@gmail.com>
2023-01-02 21:12:42 +0100

    fix bug on 1-rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -17,9 +17,9 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """ setter function to set the value of width. """
-        if not isinstance(self.__width, int):
+        if not isinstance(value, int):
             raise TypeError('width must be an integer')
-        if self.__width < 0:
+        if value < 0:
             raise ValueError('width must be >= 0')
         self.__width = value
 
@@ -31,8 +31,8 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """ setter function to set the value of height. """
-        if not isinstance(self.__height, int):
+        if not isinstance(value, int):
             raise TypeError('height must be an integer')
-        if self.__height < 0:
+        if value < 0:
             raise ValueError('height must be >= 0')
         self.__height = value
