2a8d2abef413208743fd268a81e052396818d2c1
Ndumatt1 <ndubuisimathew8@gmail.com>
2023-01-03 20:14:22 +0100

    comply to pycodestyle

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -69,4 +69,4 @@ class Rectangle:
     def __del__(self):
         """ Deletes an instance of a rectangle. """
         print("Bye rectangle...")
-        Rectangle.number_of_instances -=  1
+        Rectangle.number_of_instances -= 1
