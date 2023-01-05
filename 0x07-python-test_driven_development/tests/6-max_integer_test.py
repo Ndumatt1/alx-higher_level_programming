6616807cececdd6deca74876dc8c193b8c061c6d
Ndumatt1 <ndubuisimathew8@gmail.com>
2023-01-05 16:37:16 +0100

    add more edge cases

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -13,4 +13,7 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer([3.5, 8.9, 9.8, 1.1]), 9.8)
     def test_negative_in_list(self):
         self.assertEqual(max_integer([-1, -3, -6, -3]), -1)
-        
+    def test_only_one_negative(self):
+        self.assertEqual(max_integer([1, 2, -3, 4]), 4)
+    def test_max_in_beginning(self):
+        self.assertEqual(max_integer([9, 3, 4, 1, 8]), 9)
