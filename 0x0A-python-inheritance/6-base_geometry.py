ebfcbdfef9a0e819db3b4362367a6edec3689ca8
Ndumatt1 <ndubuisimathew8@gmail.com>
2023-01-09 17:15:12 +0100

    upload 6-base_geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" writes a class BaseGeometry. """
+
+
+class BaseGeometry:
+    """ Defines a class BaseGeometry. """
+    pass
+
+    def area(self):
+        """ raises an Exception"""
+        raise Exception('area() is not implemented')
