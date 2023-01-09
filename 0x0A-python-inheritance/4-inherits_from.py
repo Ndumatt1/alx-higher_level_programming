a3abace2bf112b663652f051d6f5dbf3469829d4
Ndumatt1 <ndubuisimathew8@gmail.com>
2023-01-09 16:58:33 +0100

    bug fix on 4-inherits_from

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -9,4 +9,4 @@ def inherits_from(obj, a_class):
     (directly or indirectly) from the specified class; otherwise False
     """
     #return isinstance(obj, a_class) and issubclass(type(obj), a_class)
-    return issubclass(type(obj), a_class)
+    return issubclass(type(obj), a_class) and type(obj) != a_class
