0d3883574ee6b70dcd60a1a9d73c9e174b1df507
Ndumatt1 <ndubuisimathew8@gmail.com>
2023-01-09 17:00:09 +0100

    pycodetyle compliance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -8,5 +8,4 @@ def inherits_from(obj, a_class):
     """ Returns True if the object is an instance of a class that inherits
     (directly or indirectly) from the specified class; otherwise False
     """
-    #return isinstance(obj, a_class) and issubclass(type(obj), a_class)
     return issubclass(type(obj), a_class) and type(obj) != a_class
