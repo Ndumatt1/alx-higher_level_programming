8fc32143ecdca90d1114180799862d176e200e67
Ndumatt1 <ndubuisimathew8@gmail.com>
2023-01-10 18:01:57 +0100

    upload 8-rectangle

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""This module writes a class BaseGeometry. """
+
+
+class BaseGeometry:
+    """ Defines a class. """
+
+    def area(self):
+        """ raises an Exception. """
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        """ validates value. """
+        self.name = name
+        if not isinstance(value, int):
+            raise TypeError('{} must be an integer'.format(self.name))
+        elif value <= 0:
+            raise ValueError('{} must be greater than 0'.format(self.name))
+        self.value = value
+
+
+class Rectangle(BaseGeometry):
+    """ Inherits from BaseGeometry """
+    def __init__(self, width, height):
+        """ Instantiate width and height"""
+        super().__init__()
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
