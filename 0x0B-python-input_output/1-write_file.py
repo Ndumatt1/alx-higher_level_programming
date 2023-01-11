3b1a581e6514a8d442ae3082014975cf31bd5438
Ndumatt1 <ndubuisimathew8@gmail.com>
2023-01-12 00:09:57 +0100

    upload 1-write_file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" This module writes a string to a text file. """
+
+
+def write_file(filename="", text=""):
+    """ Writes a string to a text file
+    Parameters:
+    filename - name of text file
+    text - string to write to file
+    """
+    with open(filename, "w", encoding="utf-8") as file:
+        file.write(text)
