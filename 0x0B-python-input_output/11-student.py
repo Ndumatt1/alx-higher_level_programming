0c48b0a61b1e9c2a74d9156f9f3256a79c1a8cd9
Ndumatt1 <ndubuisimathew8@gmail.com>
2023-01-12 16:57:59 +0100

    upload 11-student

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" This module defines a class Student. """
+
+
+class Student:
+    """ Defines a class Student. """
+    def __init__(self, first_name, last_name, age):
+        """ Instantiates public attributes. """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ retrieves a dictionary representation of a student instance"""
+        json_obj = {}
+        if attrs is None:
+            return self.__dict__
+        else:
+            for attr in attrs:
+                if hasattr(self, attr):
+                    json_obj[attr] = getattr(self, attr)
+            return json_obj
+
+    def reload_from_json(self, json):
+        """ replaces all attribute of the student instance. """
+        if json:
+            self.__dict__ = json
