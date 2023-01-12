29b90939bb2550d04c0990f8ac28383351291fb7
Ndumatt1 <ndubuisimathew8@gmail.com>
2023-01-12 14:39:59 +0100

    bug fix on 8-class_to_json

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -2,12 +2,11 @@
 """ This module returns the dictionary description with simple
 data structure for JSON serialization of an object
 """
-import json
 
 
 def class_to_json(obj):
     """ Returns the dictionary description.
     Parameters:
     obj - an instance of a class to serialized. """
-    obj = json.dumps(obj.__dict__)
+    obj = obj.__dict__
     return obj
