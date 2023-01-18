8654e4f7670984647a39bbfe22c8ba70c59fc7fa
Ndumatt1 <ndubuisimathew8@gmail.com>
2023-01-18 13:41:46 +0100

    add __init__.py file to test_models

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -5,12 +5,20 @@ from models.base import Base
 from models.rectangle import Rectangle
 
 
-class test_base(unittest.TestCase):
+class TestBaseClass(unittest.TestCase):
     """ inherits from unittest Tescases to use the unittest modules"""
+    def test_id_exist(self):
+        obj = Base()
+        obj1 = Base(12)
+        self.assertTrue(obj)
+        self.assertTrue(obj1)
+
     def test_assign_automatic(self):
         """ test to check for automatic assignment of id"""
         base = Base()
+        base2 = Base()
         self.assertEqual(base.id, 1)
+        self.assertEqual(base2.id, 2)
 
     def test_id_not_None(self):
         """ checks if id is not None"""
