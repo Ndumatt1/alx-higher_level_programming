ff4e74fdd9cd2e83136e4392d4bdf2758f2a5329
Ndumatt1 <ndubuisimathew8@gmail.com>
2023-01-20 12:20:07 +0100

    upload 100-append_after

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" This module inserts a line of text to a file"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    with open(filename, "r", encoding="utf-8") as file:
+        lines = file.readlines()
+    with open(filename, "w", encoding="utf-8") as file:
+        for line in lines:
+            file.write(line)
+            if search_string in line:
+                file.write(new_string)
