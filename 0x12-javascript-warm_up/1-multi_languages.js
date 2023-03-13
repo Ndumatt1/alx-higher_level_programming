f3f1858858ba74e4c871a0420f1e9e73901e7ed6
Ndumatt1 <ndubuisimathew8@gmail.com>
2023-03-13 21:46:19 +0100

    fix check error on 1-multi_languages

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,3 @@
 #!/usr/bin/node
-const cVar = 'C is fun';
-const pythonVar = 'Python is cool';
-const myVar = 'Javascript is amazing';
-
+const cVar = 'C is fun\nPython is cool\nJavascript is amazing';
 console.log(cVar);
-console.log(pythonVar);
-console.log(myVar);
