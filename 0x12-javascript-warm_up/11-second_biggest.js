6a8ed9f4600170d96e5918c95ce1c17d371d6653
Ndumatt1 <ndubuisimathew8@gmail.com>
2023-03-15 13:53:25 +0100

    fix error on 11-second_biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,13 +4,13 @@ const argument = process.argv;
 if (argument.length <= 3) {
   console.log('0');
 } else {
-  let max = argument[2];
+  let max = argument[0];
   for (let i = 1; i <= argument.length; i++) {
     if (argument[i] > max) {
       max = argument[i];
     }
   }
-  let secondMax = argument[2];
+  let secondMax = argument[0];
   for (let j = 1; j <= argument.length; j++) {
     if ((argument[j] > secondMax) && (argument[j] < max)) {
       secondMax = argument[j];
