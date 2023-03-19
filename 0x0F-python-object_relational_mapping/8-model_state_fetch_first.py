e770899c31a6c4fccf25cd6c25665550264ca71d
Ndumatt1 <ndubuisimathew8@gmail.com>
2023-03-19 15:17:44 +0100

    upload 8-model_state_fetch_first

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
-''' This module lists all State ibjects from the database hbtn_0e_6_usa'''
+''' This module prints the first State object
+from the database hbtn_0e_6_usa
+'''
 from model_state import Base, State
 from sqlalchemy.orm import Session
-from sqlalchemy import create_engine
 from sys import argv
+from sqlalchemy import create_engine
 
 if __name__ == '__main__':
     user = argv[1]
     pswd = argv[2]
     db = argv[3]
-
     engine = create_engine(f'mysql+mysqldb://{user}:{pswd}@localhost/{db}')
     Base.metadata.create_all(engine)
-
     session = Session(engine)
-
-    query = session.query(State).order_by(State.id).all()
-
-    for state in query:
-        print('{}: {}'.format(state.id, state.name))
+    if not State:
+        print()
+    query = session.query(State).order_by(State.id).first()
+    print('{}: {}'.format(query.id, query.name))
     session.close()
