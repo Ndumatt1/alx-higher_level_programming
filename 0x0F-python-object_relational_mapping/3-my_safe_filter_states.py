ae23b7669f91410d1889197ab8e891b719af36c1
Ndumatt1 <ndubuisimathew8@gmail.com>
2023-03-20 00:58:55 +0100

    fix pycodestyle error

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-''' 
+'''
 This module takes an argument and displays all values in the states table
 where name matches the argument. This query is safe from mysql injections
 '''
@@ -10,12 +10,13 @@ if __name__ == '__main__':
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
-    query = sys.argv[4]
+    name = sys.argv[4]
 
     connect = MySQLdb.connect('localhost', username, password, db_name)
     cursor = connect.cursor()
-
-    cursor.execute('SELECT * FROM states WHERE name = %s ORDER BY states.id', (query,))
+    query = "SELECT * FROM states WHERE name = \
+            %s ORDER BY states.id ASC"
+    cursor.execute(query, (name,))
     records = cursor.fetchall()
     for record in records:
         print(record)
