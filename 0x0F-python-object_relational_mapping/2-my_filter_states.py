3912921e84c48d188df7a3ea913dbde5312b3a1e
Ndumatt1 <ndubuisimathew8@gmail.com>
2023-03-20 00:46:38 +0100

    make 2-my_filter_states pycodestyle compliant

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-''' 
+'''
 This module takes in an argument and displays all values in the states
 table where name matches the argument
 '''
@@ -10,15 +10,13 @@ if __name__ == '__main__':
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
-    query_name = sys.argv[4]
+    name = sys.argv[4]
 
     connect = MySQLdb.connect("localhost", username, password, db_name)
     curs = connect.cursor()
-    sql = 'SELECT * FROM states WHERE name = {query} ORDER BY states.id'
-    qry = sql.format(query=query_name)
-    curs.execute(qry)
-
-    '''   curs.execute("SELECT * FROM states WHERE name = %s ORDER BY states.id").format(query_name)'''
+    sql = "SELECT * FROM states WHERE \
+        states.name = '{}' ORDER BY states.id ASC".format(name)
+    curs.execute(sql)
     records = curs.fetchall()
     for record in records:
         print(record)
