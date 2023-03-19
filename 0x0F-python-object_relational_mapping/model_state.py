b1b0980253bbddd7e180254dae26c03510569a8c
Ndumatt1 <ndubuisimathew8@gmail.com>
2023-03-19 12:37:37 +0100

    fix error on model_state

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,13 +2,13 @@
 ''' This module defines an ORM using MysqlAlchemy'''
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm import relationship
-from sqlalchemy import create_engine
-url = 'mysql://localhost:matthewlinux@3306'
+
 Base = declarative_base()
+
+
 class State(Base):
     ''' Defines a model'''
     __tablename__ = 'tables'
-    id = Column(Integer, primary_key=True, unique=True, nullable=False)
+
+    id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
