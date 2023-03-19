1fb37d2a8899bc571da1b667cf3f1e2702212278
Ndumatt1 <ndubuisimathew8@gmail.com>
2023-03-20 00:10:32 +0100

    upload model_city and 14-model_city_fetch_by_state

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+''' This module prints all City objects '''
+if __name__ == '__main__':
+    from sys import argv
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+    from model_state import Base, State
+    from model_city import City
+
+    user, pswd, db = argv[1:4]
+    engine = create_engine(f'mysql+mysqldb://{user}:{pswd}@localhost/{db}')
+    session = Session(engine)
+    query = session.query(City).join(State).order_by(City.id).all()
+    for record in query:
+        print(f'{record.state.name}: ({record.id}) {record.name}')
