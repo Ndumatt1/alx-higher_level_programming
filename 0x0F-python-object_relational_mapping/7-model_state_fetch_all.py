e25bfe355dfb1ca7e1f2bf99c6e4396b2beee43b
Ndumatt1 <ndubuisimathew8@gmail.com>
2023-03-19 14:29:06 +0100

    upload 7-model_state_fetch_all query file

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+''' This module lists all State ibjects from the database hbtn_0e_6_usa'''
+from model_state import Base, State
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+from sys import argv
+
+user = argv[1]
+passwd = argv[2]
+db_name = argv[3]
+
+engine = create_engine(f'mysql+mysqldb://{user}:{passwd}@localhost/{db_name}')
+
+session = Session(engine)
+
+query = session.query(State).order_by(State.id).all()
+
+for state in query:
+    print('{}: {}'.format(state.id, state.name))
