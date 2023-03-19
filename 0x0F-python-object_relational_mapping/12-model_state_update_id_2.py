47fb329e75f8469bfe56c9e56b9dd384863a2951
Ndumatt1 <ndubuisimathew8@gmail.com>
2023-03-19 21:28:40 +0100

    upload 12-model_state_update_id_2

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,19 +1,14 @@
 #!/usr/bin/python3
-'''
-This module adds a new object to the State model
-'''
+''' This module changes the name of a State object'''
 if __name__ == '__main__':
+    from sys import argv
+    from sqlalchemy import create_engine
     from sqlalchemy.orm import Session
     from model_state import Base, State
-    from sqlalchemy import create_engine
-    from sys import argv
 
     user, pswd, db = argv[1:4]
     engine = create_engine(f'mysql+mysqldb://{user}:{pswd}@localhost/{db}')
     session = Session(engine)
-
-    new_object = State(name='Louisiana')
-    session.add(new_object)
+    query = session.query(State).filter(State.id == 2).first()
+    query.name = 'New Mexico'
     session.commit()
-    print(new_object.id)
-    session.close()
