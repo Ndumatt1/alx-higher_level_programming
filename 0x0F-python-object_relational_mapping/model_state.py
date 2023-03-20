072952c54668e0a8a37effcaef60369f4b7d522b
Ndumatt1 <ndubuisimathew8@gmail.com>
2023-03-20 01:47:48 +0100

    fix typo error

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,13 +2,14 @@
 ''' This module defines an ORM using MysqlAlchemy'''
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import create_engine
 
 Base = declarative_base()
 
 
 class State(Base):
-    ''' Defines a model'''
-    __tablename__ = 'tables'
+    ''' Inherits from declarative_base and Defines a model'''
+    __tablename__ = 'states'
 
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
