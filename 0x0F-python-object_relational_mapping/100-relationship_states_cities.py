042074d2f31c9f70a4fae63fd02fa7395aae6ca2
Ndumatt1 <ndubuisimathew8@gmail.com>
2023-03-20 03:00:28 +0100

    upload 100-relationship

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+''' This module creates a state and a city '''
+if __name__ == '__main__':
+    from sys import argv
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
