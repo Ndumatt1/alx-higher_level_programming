36e127575a6b64220cc84b08f43e6090b2d8cdd5
Ndumatt1 <ndubuisimathew8@gmail.com>
2023-03-20 01:52:19 +0100

    fix bug on model_state

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,7 +2,6 @@
 ''' This module defines an ORM using MysqlAlchemy'''
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import create_engine
 
 Base = declarative_base()
 
@@ -11,5 +10,5 @@ class State(Base):
     ''' Inherits from declarative_base and Defines a model'''
     __tablename__ = 'states'
 
-    id = Column(Integer, primary_key=True, nullable=False)
+    id = Column(Integer, autoincrement=True, primary_key=True)
     name = Column(String(128), nullable=False)
