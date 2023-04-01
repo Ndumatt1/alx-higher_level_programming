8d0fc36d89f58ff3e4d77b6ae89518cd2d4c9b67
Ndumatt1 <ndubuisimathew8@gmail.com>
2023-04-01 21:26:19 +0100

    fix bug in 10-my_github

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -2,11 +2,20 @@
 '''
 This module takes user github credentials and uses github API to display id
 '''
-if __name__ == '__main__':
-    import requests
-    from requests.auth import HTTPBasicAuth
-    from sys import argv
+import requests
+from requests.auth import HTTPBasicAuth
+from sys import argv
+
+
+def func():
     auth = HTTPBasicAuth(argv[1], argv[2])
     with requests.get('https://api.github.com/user', auth=auth) as response:
-        value = response.json()['id']
-        print(value)
+        try:
+            value = response.json()['id']
+            print(value)
+        except KeyError:
+            return None
+
+
+if __name__ == '__main__':
+    func()
