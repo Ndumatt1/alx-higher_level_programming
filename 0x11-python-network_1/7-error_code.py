a975009016fb24c3ad13a4e402524af17fd03db8
Ndumatt1 <ndubuisimathew8@gmail.com>
2023-04-01 03:23:41 +0100

    upload 7-error_code

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+This module take in a URL, sends a request to the URL and displays
+the body of the eresponse
+'''
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+
+    with requests.get(argv[1]) as response:
+        if (response.status_code >= 400):
+            print(f'Error code: {request.status_code}')
+        else:
+            print(response.text)
