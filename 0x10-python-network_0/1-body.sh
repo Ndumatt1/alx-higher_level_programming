368d5bc8d497ac27cb495d1158e9890732c4417f
Ndumatt1 <ndubuisimathew8@gmail.com>
2023-04-02 15:11:01 +0100

    handle redirection on 1-body

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes in a URL, sends a GET request to the URL
-curl -s -L -w "%{http_code}\n" -o /dev/null "$1" | grep -q 200 && curl -s "$1"
+curl -s -w "%{http_code}\n" -o /dev/null -L "$1" | grep -q 200 && curl -s -L "$1"
